f7a0faadbc23da82d9fc3c1ba1d6e62e3ed613fa
KareemMMarey <kareem.m.marey@gmail.com>
2022-07-21 13:28:51 +0300

    Add files via upload

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private IStoreRepository repository;
+        public int PageSize = 4;
+        public HomeController(IStoreRepository repo)
         {
-            return View();
+            repository = repo;
         }
+        public ViewResult Index(int productPage = 1)
+  => View(new ProductsListViewModel
+      {
+          Products = repository.Products
+                               .OrderBy(p => p.ProductID)
+                               .Skip((productPage - 1) * PageSize)
+                               .Take(PageSize),
+          PagingInfo = new PagingInfo
+          {
+              CurrentPage = productPage,
+              ItemsPerPage = PageSize,
+              TotalItems = repository.Products.Count()
+          }
+      });
     }
 }
--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opt => { opt.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]); });
+builder.Services.AddScoped<IStoreRepository,EFStoreRepository>();   
 
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 app.UseStaticFiles();
+app.MapControllerRoute("pagination",
+ "Products/Page{productPage}",
+ new { Controller = "Home", action = "Index" });
 app.MapDefaultControllerRoute();
+SeedData.EnsurePopulated(app);
 app.Run();
